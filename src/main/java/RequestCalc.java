087a9f205c282922c2379bd0f0282b0b43bccfb3
Kadyrov23 <zloyrudik@mail.ru>
2022-05-09 16:03:03 +0500

    Created branch dev2 and created class RequestCalc

--- src/main/java/RequestCalc.java
+++ src/main/java/RequestCalc.java
@@ -1,4 +1,47 @@
+import javax.servlet.http.HttpServletRequest;
 
-public class RequestCalc {
-
-}
+public class RequestCalc extends Calculations{
+		private final String first_calc;
+		private final String second_calc;
+		private final String third_calc;
+		private String result;
+		
+		public void calc() {
+			int first_try;
+			int second_try;
+			int third_try;
+			try {
+			first_try=Integer.parseInt(first_calc);
+			second_try=Integer.parseInt(second_calc);
+			third_try=Integer.parseInt(third_calc);
+			result=Integer.toString(first_try*second_try*third_try);
+			}
+			catch (NumberFormatException e) {
+				first_try=0;
+				second_try=0;
+				third_try=0;
+			result="Некорректные данные";
+			}
+		}
+		
+		private RequestCalc (String first, String second, String third) {
+			this.first_calc = first;
+			this.second_calc = second;
+			this.third_calc = third;
+			}
+		
+		public static RequestCalc fromRequestParameters(HttpServletRequest request) {
+			return new RequestCalc(
+			request.getParameter("first"),
+			request.getParameter("second"),
+			request.getParameter("third"));
+			}
+			
+		public void setAsRequestAttributesAndCalculate(HttpServletRequest request) {
+			request.setAttribute("first_result", first_calc);
+			request.setAttribute("second_result", second_calc);
+			request.setAttribute("third_result", third_calc);
+			calc();
+			request.setAttribute("result", result);
+		}
+	}
